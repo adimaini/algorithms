4861e1ccda5b96fcdeac8732d14a641b7e7eb1dc
Adi Maini <adimaini@Adis-MBP.fios-router.home>
2020-03-27 20:40:59 -0400

    changes as per professor's remarks

--- Project_3/lcs.java
+++ Project_3/lcs.java
@@ -1,17 +1,4 @@
-import java.util.Random;
-
-public class lcs{
-	public static void main(String[] args){
-		// new object of lcs and calling the lcs calculator with sequence
-		lcs lcs_object = new lcs();
-		String[] sequences = lcs_object.sequenceGenerator();
-		int result = lcs_object.lcsCalculator(sequences);
-
-		System.out.println("LCS: " + result);
-		System.out.println("Sequence 1: " + sequences[0]);
-		System.out.println("Sequence 2: " + sequences[1]);
-	}
-
+public class LCS{
 	// calculates the longest common subsequence
 	public int lcsCalculator(String[] sequence){
 		// convert ArrayList to s1 and s2
@@ -20,8 +7,6 @@ public class lcs{
 
 		// create a 2d integer array
 		int[][] lcs = new int[s1.length() + 1][s2.length() + 1];
-
-
 
 		for (int i=1; i <= s1.length(); i++){
 			for (int j=1; j <= s2.length(); j++){
@@ -41,29 +26,5 @@ public class lcs{
 			}
 		}
 		return lcs[s1.length()][s2.length()];
-	}
-
-	// generates the two sequences to be used
-	public String[] sequenceGenerator(){
-		int n = 100;
-		String s1 = "mangoes";
-		String s2 = "manloves";
-
-		int leftLimit = 97; // letter 'a'
-	    int rightLimit = 122; // letter 'z'
-	    int targetStringLength = n;
-	    Random random = new Random();
-	 
-	    String generatedString1 = random.ints(leftLimit, rightLimit + 1)
-	    	.limit(targetStringLength)
-	    	.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-	    	.toString();
-
-    	String generatedString2 = random.ints(leftLimit, rightLimit + 1)
-	    	.limit(targetStringLength)
-	    	.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-	    	.toString();
-
-		return new String[] {generatedString1, generatedString2};
 	}
 }
