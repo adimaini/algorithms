f8d44bf5fc2d85e031227099bed871b4c01e8522
Adi Maini <adimaini@Adis-MacBook-Pro.local>
2020-04-01 12:19:17 -0400

    lcr algorithm changes to simplify

--- Project_3/src/main/java/Project_3/LCS.java
+++ Project_3/src/main/java/Project_3/LCS.java
@@ -9,24 +9,22 @@ public class LCS{
 
 		// create a 2d integer array
 		int[][] lcs = new int[s1.length() + 1][s2.length() + 1];
+		lcs[0][0] = 0;
 
 		for (int i=1; i <= s1.length(); i++){
 			for (int j=1; j <= s2.length(); j++){
-				// initialize LCS for 0
-				if ( (i==0) || (j==0) ){
-					lcs[i][j] = 0;
-					continue;
-				}
-				else lcs[0][0] = 0;
-
+				
+				// if the character does not match, then pick the max of either sequences' previous max lcs
 				if ( (s1.charAt(i-1) != s2.charAt(j-1)) ) {
 					lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
 				}
+				// else (they do match). Add one. 
 				else {
 					lcs[i][j] = lcs[i-1][j-1] + 1;
 				}
 			}
 		}
+		// return the lcs answer (bottom right most of the lcs 2d-matrix)
 		return lcs[s1.length()][s2.length()];
 	}
 }
