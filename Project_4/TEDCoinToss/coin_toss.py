608731dc755566ad7c3b15df057413143b5e7473
Adi Maini <adimaini@vt.edu>
2020-04-25 21:52:49 -0400

    connect to real QC. some time analysis changes. logic changes in print statements.

--- Project_4/TEDCoinToss/coin_toss.py
+++ Project_4/TEDCoinToss/coin_toss.py
@@ -4,38 +4,41 @@ written by Marcus Edwards on November 20, 2018
 '''
 
 from qiskit import IBMQ, QuantumCircuit, execute, Aer
+from qiskit.providers.ibmq import least_busy
 import csv
 import random
+import time
+
+#  program time tracking
+startTime = time.time()
+
 
 API_TOKEN = '7f744419bf02c5ad2935c5bad2d858f150f3793da866268cd5c0841bc531c8a72a4484d4c59bf3afa44c29e7859a875cf0634104707aa341424b662acd7c4655'
 print('\n-----------------------------------------------------')
-IBMQ.save_account(API_TOKEN)
+IBMQ.save_account(API_TOKEN, overwrite=False)
 IBMQ.load_account()
 # each provider has multiple backends
 provider = IBMQ.get_provider(hub='ibm-q', group='open', project='main')
-#asking a provider for their 
-print('\nThese are the backends available to use....\n')
-print(provider.backends())
 
-backend = provider.get_backend('ibmqx2')
-print('\n\nBackend being used: ', backend)
-print(backend.configuration().basis_gates)
+print('\nThese are the backends that are real quantum devices and are currently operational....')
+print(provider.backends(simulator=False, operational=True), '\n')
+print('\nLets pick the least busy backend....')
+backend = least_busy(provider.backends(simulator=False, operational=True))
+print('Backend being used:', backend, ', which has the following available basis gates:')
+print(backend.configuration().basis_gates, '\n')
 
 # Quantum circuit configuration
 heads_circuit = QuantumCircuit(1,1)
 tails_circuit = QuantumCircuit(1,1)
-
 #  heads circuit config
 heads_circuit.h(0)
-heads_circuit.x(0)
 heads_circuit.h(0)
 heads_circuit.measure(range(1), range(1))
-
 #  tails circuit config
 tails_circuit.h(0)
+tails_circuit.x(0)
 tails_circuit.h(0)
 tails_circuit.measure(range(1), range(1))
-
 #  decompose our original quantum circuits to quantum computer backend's basis
 heads_basis = heads_circuit.decompose()
 tails_basis = tails_circuit.decompose()
@@ -69,16 +72,15 @@ def run_challenge(trials, flipped, device):
             heads += 1
 
     if heads>0:
-        heads_job_results = execute(heads_basis, backend).result()
+        heads_job_results = execute(heads_basis, backend, shots=heads).result()
         heads_job = heads_job_results.get_counts(heads_circuit)
         print('\nHeads Job results:\n')
         print( heads_job_results, '\n')
-
     else:
         heads_job = 0
 
     if tails>0:
-        tails_job_results = execute(tails_basis, backend).result()
+        tails_job_results = execute(tails_basis, backend, shots=tails).result()
         tails_job = tails_job_results.get_counts(tails_circuit)
         print('\nTails Job results:\n')
         print(tails_job_results, '\n')
@@ -108,6 +110,7 @@ print('{0} people are playing a game against IBMs quantum computer.'.format(flip
 print('{0} people chose to flip.'.format(flips))
 
 exp_heads, exp_tails, heads, tails = run_challenge(flips, flipped=True, device='ibmqx4') #run protocol for each flip
+print(exp_heads, '\n', exp_tails)
 
 p_heads = heads/(heads+tails)
 p_tails = tails/(heads+tails)
@@ -116,23 +119,24 @@ print('{0} people who flipped got heads.'.format(heads))
 print('{0} people who flipped got tails.'.format(tails))
 
 print('\nPeople who flipped got the following results:')
-print("state     probability")
+print("state         Probability")
 print("heads         {0}".format(p_heads))
 print("tails         {0}".format(p_tails))
-                       
-print('End result from quantum computer after these flips:')
-# results = combine_results(exp_heads, exp_tails, p_heads, p_tails)
-
-print("state         Solution Distribution")
+                  
+print('End result after the quantum computer flips after the human has flipped:')
+print("state         Probability")
 print("heads         {0}".format((exp_heads['0']+exp_tails['0'])/flips))
-print("tails         0.0")
+print("tails         {0}".format((flips - (exp_heads['0']+exp_tails['0'])) / flips))
 
 print('\n{0} people chose to pass.'.format(passes))
 
 exp_heads, exp_tails, heads, tails = run_challenge(passes, flipped=False, device='ibmqx4') #run protocol for each pass
+print(exp_heads, '\n', exp_tails)
 
-print('End result from quantum computer after these passes:')
-print("state         Solution Distribution")
+print('End result after the quantum computer flips after the human passes:')
+print("state         Probability")
 print("heads         {0}".format(exp_heads['0']/passes))
-print("tails         {0}".format(exp_tails))
+print("tails         {0}".format((passes-exp_heads['0'])/passes))
+
+print('Program time is %.2f'.format(time.time()-startTime))
 
